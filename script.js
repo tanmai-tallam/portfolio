6f471ecff5d5db799bfcea37f3ff4a92d4870bf9
Tanmai Tallam <tanmai.tallam@gmail.com>
2024-05-23 18:33:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var typed= new Typed(".multiple-text",{
+    strings: ["FrontEnd Developer ", "Backend Developer" , "Graphic Designer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay: 1000,
+    loop:true
+})
+var typed= new Typed(".multiple-text",{
+    strings: ["FrontEnd Developer ", "Backend Developer" , "Graphic Designer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay: 1000,
+    loop:true
+})
