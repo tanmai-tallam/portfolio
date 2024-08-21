3fae4596ddb1e64b1764dbe2370064ceae1108a1
Tanmai Tallam <tanmai.tallam@gmail.com>
2024-08-21 15:16:58 +0530

    commit changes

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var typed= new Typed(".multiple-text",{
-    strings: ["FrontEnd Developer ", "Backend Developer" , "Graphic Designer"],
+    strings: ["FrontEnd Development ", "Machine Learning" , "Data Structures and Algorithms"],
     typeSpeed:100,
     backSpeed:100,
     backDelay: 1000,
     loop:true
 })
 var typed= new Typed(".multiple-text",{
-    strings: ["FrontEnd Developer ", "Backend Developer" , "Graphic Designer"],
+    strings: ["FrontEnd Development ", "Machine Learning" , "Data Structures and Algorithms"],
     typeSpeed:100,
     backSpeed:100,
     backDelay: 1000,
